c79da848a3449d416dbb2ce27b2b64ff9417f153
rezaseydanloo <rezaseydanloo.83@gmail.com>
2024-05-01 23:24:49 +0330

    update Normal Usre Class (create his attribute and getter)

---
+++ Library/NormalUser.java
@@ -0,0 +1,38 @@
+package Library;
+
+public class NormalUser {
+
+    String FirstName ;
+    String LastName ;
+    int UserId ;
+    int UserPhoneNumber ;
+    String RegistrationTime ;
+
+    public NormalUser(String firstName, String lastName, int userId, int userPhoneNumber, String registrationTime) {
+        FirstName = firstName;
+        LastName = lastName;
+        UserId = userId;
+        UserPhoneNumber = userPhoneNumber;
+        RegistrationTime = registrationTime;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public int getUserId() {
+        return UserId;
+    }
+
+    public int getUserPhoneNumber() {
+        return UserPhoneNumber;
+    }
+
+    public String getRegistration() {
+        return RegistrationTime;
+    }
+}
