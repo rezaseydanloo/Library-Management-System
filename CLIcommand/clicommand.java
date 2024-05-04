506f7c4f2b39f162960cc6d6b459af742f9f4916
rezaseydanloo <rezaseydanloo.83@gmail.com>
2024-05-05 01:38:39 +0330

    ClIcommand class class updated ( (was a new line) compeleted line 8)

--- CLIcommand/clicommand.java
+++ CLIcommand/clicommand.java
@@ -10,12 +10,13 @@ public class clicommand {
     public void cli() {
 
         System.out.println("|-lib add book <name> <author> <Description>     :Add a new book to the library.");
-        System.out.println("|-lib get hrs                                    :Retrieve library operating hours.");
+        System.out.println("|-lib get hrs                                    :Retrieve library operating hours (super admin privilege required).");
         System.out.println("|-lib add member <memberID> <password>           :Add a new member to the library (admin privilege required).");
-        System.out.println("|-lib rent <bookName> <memberName> <memberID>    :Rent a book for a specific member.");
+        System.out.println("|-lib rent <bookName> <UserName> <memberID>      :Rent a book for a specific member.");
         System.out.println("|-lib get available books                        :View available books for rental.");
         System.out.println("|-lib remove member <memberID>                   :Remove a member from the library (admin privilege required).");
         System.out.println("|-lib return <bookName>                          :Return a rented book to the library.");
+        System.out.println("|-Find user ID <FirstName> <LastName>            :To find ID if you forget it.");
         System.out.println("|-command help for line ?                        :example: help line 1");
 
         Scanner input = new Scanner(System.in);
@@ -32,11 +33,15 @@ public class clicommand {
             Pattern Line1 = Pattern.compile("^(lib)\\s+(add)\\s+(book)\\s+<([a-zA-Z\\sa-zA-z]+)>\\s+" +
                     "<([a-zA-Z0-9\\sa-zA-z0-9]+)>\\s+<([a-zA-Z0-9\\sa-zA-z0-9]+)>", Pattern.CASE_INSENSITIVE);
             Pattern Line2 = Pattern.compile("^(lib)\\s+(get)\\s+(hrs)", Pattern.CASE_INSENSITIVE);
-            Pattern Line3 = Pattern.compile("^(lib)\\s+(add)\\s+(member)\\s+<([0-9])>\\s+<([a-zA-Z0-9\\sa-zA-z0-9]+)>", Pattern.CASE_INSENSITIVE);
-            Pattern Line4 = Pattern.compile("^(lib)\\s+(rent)\\s+<([a-zA-Z0-9\\sa-zA-z0-9]+)>\\s+<([a-zA-Z\\sa-zA-z]+)>\\s+<([0-9]+)>", Pattern.CASE_INSENSITIVE);
+            Pattern Line3 = Pattern.compile("^(lib)\\s+(add)\\s+(member)\\s+<([0-9])>\\s+<([a-zA-Z0-9\\sa-zA-z0-9]+)>",
+                    Pattern.CASE_INSENSITIVE);
+            Pattern Line4 = Pattern.compile("^(lib)\\s+(rent)\\s+<([a-zA-Z0-9\\sa-zA-z0-9]+)>\\s+<([a-zA-Z\\sa-zA-z]+)>" +
+                    "\\s+<([0-9]+)>", Pattern.CASE_INSENSITIVE);
             Pattern Line5 = Pattern.compile("^(lib)\\s+(get)\\s+(available)\\s+(books)", Pattern.CASE_INSENSITIVE);
             Pattern Line6 = Pattern.compile("^(lib)\\s+(remove)\\s+(member)\\s+<([0-9]+)>", Pattern.CASE_INSENSITIVE);
             Pattern Line7 = Pattern.compile("^(lib)\\s+(return)\\s+<([a-zA-Z\\sa-zA-z]+)>", Pattern.CASE_INSENSITIVE);
+            Pattern Line8 = Pattern.compile("^(Find)\\s+(user)\\s+(ID)\\s+<([a-zA-Z\\sa-zA-z]+)>\\s+" +
+            "<([a-zA-Z0-9\\sa-zA-z0-9]+)>");
 
             Matcher line1 = Line1.matcher(inputString);
             Matcher line2 = Line2.matcher(inputString);
@@ -45,6 +50,7 @@ public class clicommand {
             Matcher line5 = Line5.matcher(inputString);
             Matcher line6 = Line6.matcher(inputString);
             Matcher line7 = Line7.matcher(inputString);
+            Matcher line8 = Line8.matcher(inputString);
 
             Library LibraryObject_CLI = new Library();
 
@@ -83,6 +89,13 @@ public class clicommand {
 
                 System.out.println("line 7 dorost");
             }
+
+            else if (line8.find()){
+
+                LibraryObject_CLI.IdFinder(line8.group(4),line8.group(5));
+
+            }
+
             else
                 System.out.println("|-! something went wrong , try again ");
 
