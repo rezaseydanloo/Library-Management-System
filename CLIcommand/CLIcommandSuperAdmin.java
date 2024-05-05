7e161fdf41a8d86de956f27a61bd933677a478bf
rezaseydanloo <rezaseydanloo.83@gmail.com>
2024-05-05 22:02:12 +0330

    update super admin cli class ( copleted option )

--- CLIcommand/CLIcommandSuperAdmin.java
+++ CLIcommand/CLIcommandSuperAdmin.java
@@ -12,8 +12,8 @@ public class CLIcommandSuperAdmin {
 
     public void SuperCLI (){
 
-        System.out.println("|---super admin log in page - ");
-        System.out.println("|-- log in as super admin for running the program -");
+        System.out.println("|-----super admin log in page ");
+        System.out.println("|-- description : log in as super admin for running the program ");
         System.out.println("|-- <FirstName> <LastName> <Password> ");
 
         Pattern inputPattern = Pattern.compile("<reza>\\s*<seydanloo>\\s*<912138364761383>",Pattern.CASE_INSENSITIVE);
@@ -23,17 +23,47 @@ public class CLIcommandSuperAdmin {
 
         while (true){
 
-
             System.out.print("|>> ");
             String  inputString = input.nextLine();
             Matcher inputMatcher = inputPattern.matcher(inputString);
 
-            if (inputMatcher.find()){
+            if (inputMatcher.find()) {
 
-                System.out.println("|- accepted ");
-                System.out.println("----------------------------------------------------");
+                System.out.println("|-^-- accepted ");
+                System.out.println("|----------------------------------------------------");
+                System.out.println("|-----option ");
+                System.out.println("|-1- go to cli (user command)      : <cli>");
+                System.out.println("|-2- go to super admin panel       : <panel>");
 
-                startclicommand.cli();
+                Pattern pattern1 = Pattern.compile("<cli>", Pattern.CASE_INSENSITIVE);
+                Pattern pattern2 = Pattern.compile("<panel>", Pattern.CASE_INSENSITIVE);
+
+                while (true) {
+                    System.out.print("|>> ");
+                    inputString = input.nextLine();
+
+                    Matcher matcher1 = pattern1.matcher(inputString);
+                    Matcher matcher2 = pattern2.matcher(inputString);
+
+
+                    if (matcher1.find()) {
+
+                        System.out.println("|----------------------------------------------------");
+                        startclicommand.cli();
+
+                    }
+
+                    else if (matcher2.find()) {
+
+                        System.out.println("|----------------------------------------------------");
+                        SuperAdminPanel();
+                    }
+
+                    else
+                        System.out.println("|-! something went wrong , try again ");
+
+
+                }
             }
 
             else
@@ -46,16 +76,17 @@ public class CLIcommandSuperAdmin {
 
     public void SuperAdminPanel (){
 
-        System.out.println("|---super admin panel -");
-        System.out.println("|-- add admin                       : write <add>   ");
-        System.out.println("|-- remove admin                    : write <remove>");
-        System.out.println("|-- get hrs                         : write <hrs>   ");
-        System.out.println("|-- go to cli (user command)        : write <cli>   ");
+        System.out.println("|-----super admin panel ");
+        System.out.println("|-- add admin                       : <add>   ");
+        System.out.println("|-- remove admin                    : <remove>");
+        System.out.println("|-- get hrs                         : <hrs>   ");
+        System.out.println("|-- go to cli (user command)        : <cli>   ");
 
         Pattern AddPattern = Pattern.compile("add" , Pattern.CASE_INSENSITIVE);
         Pattern RemovePattern = Pattern.compile("remove" , Pattern.CASE_INSENSITIVE);
         Pattern HrsPattern = Pattern.compile("hrs" , Pattern.CASE_INSENSITIVE);
         Pattern CliPattern = Pattern.compile("cli" , Pattern.CASE_INSENSITIVE);
+
 
     }
 
