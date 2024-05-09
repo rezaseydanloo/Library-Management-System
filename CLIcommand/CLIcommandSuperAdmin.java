c9850b777b42ca223b874b8ee42e19fef9beedf8
rezaseydanloo <rezaseydanloo.83@gmail.com>
2024-05-09 12:28:01 +0330

    debug regex , regex add admin

--- CLIcommand/CLIcommandSuperAdmin.java
+++ CLIcommand/CLIcommandSuperAdmin.java
@@ -1,6 +1,7 @@
 package CLIcommand;
 
-import javax.crypto.spec.PSource;
+import Library.Library;
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,27 +11,25 @@ public class CLIcommandSuperAdmin {
     clicommand startclicommand  = new clicommand();
 
 
-    public void SuperCLI (){
+    public void SuperLogIn(){
 
         System.out.println("|-----super admin log in page ");
         System.out.println("|-- description : log in as super admin for running the program ");
         System.out.println("|-- <FirstName> <LastName> <Password> ");
 
         Pattern inputPattern = Pattern.compile("<reza>\\s*<seydanloo>\\s*<912138364761383>",Pattern.CASE_INSENSITIVE);
-        Pattern inputAnswerPatternYes = Pattern.compile("yes",Pattern.CASE_INSENSITIVE);
-        Pattern inputAnswerPatternNo = Pattern.compile("no",Pattern.CASE_INSENSITIVE);
         Scanner input = new Scanner(System.in);
 
         while (true){
 
-            System.out.print("|>> ");
+            System.out.print("|->> ");
             String  inputString = input.nextLine();
             Matcher inputMatcher = inputPattern.matcher(inputString);
 
             if (inputMatcher.find()) {
 
                 System.out.println("|-^-- accepted ");
-                System.out.println("|----------------------------------------------------");
+                System.out.println("|----------------------------------------------------------");
                 System.out.println("|-----option ");
                 System.out.println("|-1- go to cli (user command)      : <cli>");
                 System.out.println("|-2- go to super admin panel       : <panel>");
@@ -39,7 +38,7 @@ public class CLIcommandSuperAdmin {
                 Pattern pattern2 = Pattern.compile("<panel>", Pattern.CASE_INSENSITIVE);
 
                 while (true) {
-                    System.out.print("|>> ");
+                    System.out.print("|->> ");
                     inputString = input.nextLine();
 
                     Matcher matcher1 = pattern1.matcher(inputString);
@@ -48,14 +47,14 @@ public class CLIcommandSuperAdmin {
 
                     if (matcher1.find()) {
 
-                        System.out.println("|----------------------------------------------------");
+                        System.out.println("|----------------------------------------------------------");
                         startclicommand.cli();
 
                     }
 
                     else if (matcher2.find()) {
 
-                        System.out.println("|----------------------------------------------------");
+                        System.out.println("|----------------------------------------------------------");
                         SuperAdminPanel();
                     }
 
@@ -74,18 +73,77 @@ public class CLIcommandSuperAdmin {
 
     }
 
+    //------------------------------------------------------------------------------------------------------------------
+
+
     public void SuperAdminPanel (){
 
         System.out.println("|-----super admin panel ");
-        System.out.println("|-- add admin                       : <add>   ");
-        System.out.println("|-- remove admin                    : <remove>");
+        System.out.println("|-- add admin                       : <NewAdminFirstName> <..LastName> <Password>   ");
+        System.out.println("|-- remove admin                    : <AdminID>");
         System.out.println("|-- get hrs                         : <hrs>   ");
+        System.out.println("|-- admins list Name , Id           : <list>  ");
         System.out.println("|-- go to cli (user command)        : <cli>   ");
 
-        Pattern AddPattern = Pattern.compile("add" , Pattern.CASE_INSENSITIVE);
-        Pattern RemovePattern = Pattern.compile("remove" , Pattern.CASE_INSENSITIVE);
+        Pattern AddPattern = Pattern.compile("<([a-zA-Z\\sa-zA-Z]+)>\\s*<([a-zA-Z\\sa-zA-Z]+)>\\s*<([a-zA-Z0-9]+)>" , Pattern.CASE_INSENSITIVE);
+        Pattern RemovePattern = Pattern.compile("<([0-9]{3,10})>" , Pattern.CASE_INSENSITIVE);
         Pattern HrsPattern = Pattern.compile("hrs" , Pattern.CASE_INSENSITIVE);
         Pattern CliPattern = Pattern.compile("cli" , Pattern.CASE_INSENSITIVE);
+        Pattern ListPattern = Pattern.compile("list" , Pattern.CASE_INSENSITIVE);
+
+        Scanner input = new Scanner(System.in);
+        Library LibraryObject = new Library();
+
+        while (true){
+
+            System.out.print("|->> ");
+
+            String inputString = input.nextLine();
+
+            Matcher AddMatcher = AddPattern.matcher(inputString);
+            Matcher RemoveMatcher = RemovePattern.matcher(inputString);
+            Matcher HrsMatcher = HrsPattern.matcher(inputString);
+            Matcher CliMatcher = CliPattern.matcher(inputString);
+            Matcher ListMatcher = ListPattern.matcher(inputString);
+
+            if (AddMatcher.find()){
+
+
+
+                LibraryObject.add_admin(AddMatcher.group(1),AddMatcher.group(2),AddMatcher.group(3));
+                System.out.println("|-^-- accepted");
+
+            }
+
+            else if (RemoveMatcher.find()) {
+
+
+            }
+
+            else if (HrsMatcher.find()) {
+
+
+            }
+
+            else if (CliMatcher.find()) {
+
+                System.out.println("|----------------------------------------------------------");
+                startclicommand.cli();
+
+            }
+
+            else if (ListMatcher.find()){
+
+                System.out.println("|----------------------------------------------------------");
+                LibraryObject.AdminIdFinder();
+
+            }
+
+            else
+                System.out.println("|-! something went wrong , try again ");
+
+
+        }
 
 
     }
